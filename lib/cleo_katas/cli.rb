c56444288b6252e3c434caa3d073183844f65750
Bodacious <gavin@gavinmorrice.com>
2024-11-10 23:30:57 +0000

    Update RuboCop configuration

--- lib/cleo_katas/cli.rb
+++ lib/cleo_katas/cli.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require "thor"
+require 'thor'
 module CleoKatas
   class CLI < Thor
-    desc "list", "List all available katas"
+    desc 'list', 'List all available katas'
     def list
-      puts "Listing katas..."
-      Dir.glob(File.join(Dir.pwd, "katas", "*")).each do |kata|
-        puts kata.split("/").last
+      puts 'Listing katas...'
+      Dir.glob(File.join(Dir.pwd, 'katas', '*')).each do |kata|
+        puts kata.split('/').last
       end
     end
   end
