5a3ac796637dd15ca8a27a5a16c29cb0dc3127cf
Bodacious <gavin@gavinmorrice.com>
2024-11-10 23:43:34 +0000

    Add a CLI command to create a new kata for current user

--- lib/cleo_katas/cli.rb
+++ lib/cleo_katas/cli.rb
@@ -1,14 +1,32 @@
 # frozen_string_literal: true
 
 require 'thor'
+
 module CleoKatas
   class CLI < Thor
+    include Thor::Actions
+
+    source_root File.expand_path('../templates', __FILE__)
     desc 'list', 'List all available katas'
     def list
-      puts 'Listing katas...'
+      say 'Listing katas...'
       Dir.glob(File.join(Dir.pwd, 'katas', '*')).each do |kata|
-        puts kata.split('/').last
+        say kata.split('/').last
       end
+    end
+
+    desc 'attempt DIRECTORY', 'Create a new kata attempt in the specified directory with a subdirectory named after the current user'
+    def attempt(kata)
+      username = `whoami`.strip.downcase
+      target_directory = File.join(Dir.pwd, 'katas', kata, username)
+
+      empty_directory(target_directory)
+
+      target_file = File.join(target_directory, 'main.rb')
+
+      template('main.rb.erb', target_file)
+
+      say "Created new kata attempt directory at #{target_directory} with a main.rb file"
     end
   end
 end
