7dd5af81e32909bff2d0a23c8d528b3b1ba2eca2
Bodacious <gavin@gavinmorrice.com>
2024-11-10 23:27:51 +0000

    Add RuboCop to the bundle

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@
 source "https://rubygems.org"
 
 gem "thor", "~> 1.3"
+
+gem "rubocop", "~> 1.68"
--- lib/cleo_katas/cli.rb
+++ lib/cleo_katas/cli.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
-require 'thor'
+
+require "thor"
 module CleoKatas
-  class CLI <Thor
+  class CLI < Thor
     desc "list", "List all available katas"
     def list
       puts "Listing katas..."
-      Dir.glob(File.join(Dir.pwd, "katas","*")).each do |kata|
+      Dir.glob(File.join(Dir.pwd, "katas", "*")).each do |kata|
         puts kata.split("/").last
       end
     end
