d7ca56b5637122557c22627527e253acccb0a494
Bodacious <gavin@gavinmorrice.com>
2024-11-12 16:21:24 +0000

    Basic working version of Rock Paper Scissors

--- katas/rock_paper_scissors/bodacious/main.rb
+++ katas/rock_paper_scissors/bodacious/main.rb
@@ -1,19 +1,49 @@
-require 'bundler/inline'
+# frozen_string_literal: true
 
+require 'bundler/inline'
 gemfile do
   source 'https://rubygems.org'
   gem 'matrix'
   gem 'concurrent-ruby'
+  gem 'singleton'
 end
 
-class Competition
 
-  MOVES = ['Rock','Paper', 'Scissor']
+class Simulation
+  include Singleton
+  attr_accessor :total_generations
+  attr_accessor :initial_rocks
+  attr_accessor :initial_paper
+  attr_accessor :initial_scissors
+
+end
+
+require 'optparse'
+OptionParser.new do |opts|
+  opts.banner = 'Usage: ruby main.rb [options]'
+
+  opts.on('-gGENERATIONS', '--generations=GENERATIONS', Integer, 'Number of generations') do |generations|
+    Simulation.instance.total_generations = generations
+  end
+
+  opts.on('--rROCK', '--rock=ROCK', Integer, 'Initial number of rocks') do |rocks|
+    Simulation.instance.initial_rocks = rocks
+  end
+  opts.on('--pPAPER', '--paper=PAPER', Integer, 'Initial number of paper') do |paper|
+    Simulation.instance.initial_paper = paper
+  end
+  opts.on('--sSCISSOR', '--scissor=SCISSOR', Integer, 'Initial number of scissors') do |scissors|
+    Simulation.instance.initial_scissors = scissors
+  end
+end.parse!
+
+class Competition
+  MOVES = %w[Rock Paper Scissor].freeze
 
   OUTCOMES = Matrix[
-    [:draw, :b, :a],
-    [:a, :draw, :b],
-    [:b, :a, :draw]
+    %i[draw b a],
+    %i[a draw b],
+    %i[b a draw]
   ]
 
   attr_reader :competitor_a, :competitor_b
@@ -24,9 +54,22 @@ class Competition
   end
 
   def outcome
-    index1 = MOVES.index competitor_a
-    index2 = MOVES.index competitor_b
+    index1 = MOVES.index competitor_a.class_name
+    index2 = MOVES.index competitor_b.class_name
     OUTCOMES[index1, index2]
+  end
+
+  def run!
+    case outcome
+
+    when :b then competitor_a.reproduce!
+    when :a then competitor_b.reproduce!
+    when :draw
+      competitor_a.partial_reproduce!
+      competitor_b.partial_reproduce!
+    end
+    competitor_a.die!
+    competitor_b.die!
   end
 end
 
@@ -35,16 +78,15 @@ module Competitor
     self.class.name
   end
 
-  def compete(opponent)
-    case Competition.new(self.class_name, opponent.class_name).outcome
+end
 
-    when :b then decrement_population
-    when :a then increment_population(2)
-    when :draw
-      increment_population(1)
-      increment_population(1)
-    end
+class Competition
+  attr_reader :competitor_a, :competitor_b
+  def initialize(competitor_a, competitor_b)
+    @competitor_a = competitor_a
+    @competitor_b = competitor_b
   end
+
 end
 
 module Populatable
@@ -58,12 +100,18 @@ module Populatable
     end
   end
 
-  def increment_population(count=1)
+  def increment_population(count = 1)
     self.class.population_counter.increment(count)
   end
 
-  def decrement_population(count=1)
+  def decrement_population(count = 1)
     self.class.population_counter.decrement(count)
+  end
+  alias die! decrement_population
+  alias partial_reproduce! increment_population
+
+  def reproduce!
+    self.class.population_counter.increment(2)
   end
 end
 
@@ -98,14 +146,22 @@ class PopulationCounter
   end
 end
 
+class Generation
+  def run
+    total_population = Rock.population_counter.count.times.map { Rock.new} + Paper.population_counter.count.times.map { Paper.new} + Scissor.population_counter.count.times.map { Scissor.new}
+    until total_population.size < 2
+      random_pair = total_population.sample(2)
+      total_population.delete(random_pair.first)
+      total_population.delete(random_pair.last)
+      Competition.new(random_pair.first, random_pair.last).run!
+    end
+  end
+end
 Rock.population_counter.increment(100)
 Paper.population_counter.increment(100)
 Scissor.population_counter.increment(100)
 
-Rock.new.compete(Paper.new)
-Rock.new.compete(Scissor.new)
-Paper.new.compete(Scissor.new)
-
+Simulation.instance.total_generations.times { Generation.new.run }
 puts "Rocks: #{Rock.population_counter.count}"
 puts "Papers: #{Paper.population_counter.count}"
 puts "Scissors: #{Scissor.population_counter.count}"
