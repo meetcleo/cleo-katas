aa1d0d37000d4f86a3ed079cb5efbfb5600b4315
Gavin Morrice <gavin@gavinmorrice.com>
2024-11-13 14:28:23 +0000

    Fix Rubocop complaints

--- lib/cleo_katas/configuration.rb
+++ lib/cleo_katas/configuration.rb
@@ -9,23 +9,24 @@ module CleoKatas
       super(YAML.load_file('.cleo-katas.yml'))
     end
 
-    # The preferred username for this User. Will fall back to their git config username or their system username if not set
+    # The preferred username for this User. Will fall back to their git config username or their
+    # system username if not set.
     #
     # @return [String]
     def username
-      [ 
+      [
         self['username'],
         git_username,
-        whoami,
+        whoami
       ].reject(&:empty?).first
     end
 
-    private 
+    private
 
     # The configured git username for the current git context (probably ~/.gitconfig)
     #
     # @return [String]
-    def git_username 
+    def git_username
       `git config --get user.username`.strip.downcase
     end
 
