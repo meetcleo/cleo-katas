89468f1d799defc1bf4282a26874e7c2a888f497
Fell Sunderland <fell@meetcleo.com>
2025-01-24 11:59:41 +0000

    Add tests for attrs

--- katas/4-gilded_traffic_light/fell/test/traffic_light_test.rb
+++ katas/4-gilded_traffic_light/fell/test/traffic_light_test.rb
@@ -4,6 +4,40 @@ require_relative '../traffic_light'
 require "minitest/autorun"
 
 class TrafficLightTest < Minitest::Test
+  def test_direction
+    light = TrafficLight.new(direction: 'Widdershins', timer: 55, state: 'green')
+
+    assert_equal 'Widdershins', light.direction
+  end
+
+  def test_state
+    light = TrafficLight.new(direction: 'Widdershins', timer: 55, state: 'green')
+
+    # reader
+    assert_equal 'green', light.state
+
+    # writer
+    light.state = 'test_state'
+    assert_equal 'test_state', light.state
+  end
+
+  def test_timer
+    light = TrafficLight.new(direction: 'Widdershins', timer: 55, state: 'green')
+
+    # reader
+    assert_equal 55, light.timer
+
+    # writer
+    light.timer = 101
+    assert_equal 101, light.timer
+  end
+
+  def test_state
+    light = TrafficLight.new(direction: 'Widdershins', timer: 55, state: 'green')
+
+    assert_equal 'green', light.state
+  end
+
   def test_direction_to_s
     light = TrafficLight.new(direction: 'Widdershins', timer: 55, state: 'green')
     light.to_s
