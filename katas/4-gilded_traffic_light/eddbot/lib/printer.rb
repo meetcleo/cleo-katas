fa5c275650e5562ff077885dcbef642bfb90b90d
Edd Sansome <edward.s@meetcleo.com>
2025-02-12 14:53:36 +0000

    add colorizer

--- katas/4-gilded_traffic_light/eddbot/lib/printer.rb
+++ katas/4-gilded_traffic_light/eddbot/lib/printer.rb
@@ -5,21 +5,14 @@ class Printer
 
   def print_traffic_lights(lights:)
     lights.each do |light|
-      # Quick inline way to colour states, done in a messy way rather than a dedicated method.
-      state_colour = case light[:state]
-                     when "red" then "\e[31m#{light[:state]}\e[0m"
-                     when "green" then "\e[32m#{light[:state]}\e[0m"
-                     when "amber" then "\e[33m#{light[:state]}\e[0m"
-                     else light[:state]
-                     end
-      puts("Direction: #{light[:direction]}, State: #{state_colour}, Time left: #{light[:timer]}s")
+      puts("Direction: #{light[:direction]}, State: #{state_colour(state: light[:state])}, Time left: #{light[:timer]}s")
     end
   end
 
   def print_pedestrian_signals(signals:)
     puts "\nPedestrian signals:"
     signals.each do |direction, can_walk|
-      signal_text = can_walk ? "\e[32mWALK\e[0m" : "\e[31mDON'T WALK\e[0m"
+      signal_text = can_walk ? colourizer(:green, "WALK") : colourizer(:red, "DON'T WALK")
       puts "  #{direction}: #{signal_text}"
     end
 
@@ -27,6 +20,18 @@ class Printer
   end
 
   private
+
+  def state_colour(state:) = colourizer(state.to_sym, state)
+
+  def colourizer(color, input)
+    case color
+    when :red then "\e[31m#{input}\e[0m"
+    when :green then "\e[32m#{input}\e[0m"
+    when :amber then "\e[33m#{input}\e[0m"
+    else
+      input
+    end
+  end
 
   attr_reader :out
 end
