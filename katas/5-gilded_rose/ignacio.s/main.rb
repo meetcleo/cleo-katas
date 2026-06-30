bf598fe78a298f1af840342c7dfdcd9ef11c9fae
Ignacio Garrido Sevilla <ignacio.s@meetcleo.com>
2026-06-30 14:58:16 +0100

    Refactor normal

--- katas/5-gilded_rose/ignacio.s/main.rb
+++ katas/5-gilded_rose/ignacio.s/main.rb
@@ -27,51 +27,7 @@ class GildedRose
     return sulfuras if @name == "Sulfuras, Hand of Ragnaros"
     return backstage if @name == "Backstage passes to a TAFKAL80ETC concert"
     return conjured if @name == "Conjured Mana Cake"
-
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
-      end
-    end
-
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
-      end
-    end
+    normal
   end
 
   def brie
@@ -99,6 +55,13 @@ class GildedRose
   def conjured
     @quality = @quality - 2
     @quality = @quality - 2 if @days_remaining <= 0
+    @quality = 0 if @quality < 0
+    @days_remaining = @days_remaining - 1
+  end
+
+  def normal
+    @quality = @quality - 1
+    @quality = @quality - 1 if @days_remaining <= 0
     @quality = 0 if @quality < 0
     @days_remaining = @days_remaining - 1
   end
