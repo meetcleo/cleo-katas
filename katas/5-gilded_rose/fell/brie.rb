3a8f846c9650dcdc15eadfed553ce9f092739215
Fell Sunderland <fell@meetcleo.com>
2026-06-30 14:41:34 +0100

    Extract Brie quality logic to Brie class
    Aged brie increases in quality over time and caps
    out at a maximum of 50.

--- katas/5-gilded_rose/fell/brie.rb
+++ katas/5-gilded_rose/fell/brie.rb
@@ -2,4 +2,12 @@ require_relative 'product'
 
 class Brie < Product
   NAME = "Aged Brie".freeze
+  MAX_QUALITY = 50
+  DAILY_INCREASE = 2
+
+  def tick
+    @days_remaining = days_remaining - 1
+
+    @quality = (quality + DAILY_INCREASE).clamp(0, MAX_QUALITY)
+  end
 end
--- katas/5-gilded_rose/fell/product.rb
+++ katas/5-gilded_rose/fell/product.rb
@@ -8,7 +8,7 @@ class Product
   end
 
   def tick
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
+    if @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
         @quality = @quality - 1
       end
@@ -33,18 +33,12 @@ class Product
     @days_remaining = @days_remaining - 1
 
     if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            @quality = @quality - 1
-          end
-        else
-          @quality = @quality - @quality
+      if @name != "Backstage passes to a TAFKAL80ETC concert"
+        if @quality > 0
+          @quality = @quality - 1
         end
       else
-        if @quality < 50
-          @quality = @quality + 1
-        end
+        @quality = @quality - @quality
       end
     end
   end
